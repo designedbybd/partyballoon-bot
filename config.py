cbf21b4b24ab4fc60882648b830d5820738aba70
Mohamed Barakat | Designer <112909878+designedbybd@users.noreply.github.com>
2026-08-03 14:48:52 +0300

    Remove redundant line for MINI_APP_URL

--- config.py
+++ config.py
@@ -31,4 +31,3 @@ CHANNEL_ID = "-1004368547666"
 # وتحط الرابط هنا أو في متغير بيئة/سيكريت اسمه MINI_APP_URL
 # مثال: https://designedbybd.github.io/partyballoon-bot/
 MINI_APP_URL = os.getenv("MINI_APP_URL", "")
-
